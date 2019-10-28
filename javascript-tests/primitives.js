ef6dba177ec744754f030e1f2b06c61ba4a14b2e
Manwinder Singh on School Machine <manwinder.singh@bellevuecollege.edu>
2019-10-28 11:42:47 -0700

    in class

---
+++ javascript-tests/primitives.js
@@ -0,0 +1,8 @@
+const myString = "Learning Javascript";
+const myFirstNumericVariable = 3;
+
+var oldStyle = "This is the way we used to do it.";
+let newChangableStyle = "This allows you to declare a variable that can be changed.";
+const constantVariable = "This variable can not be changed. Except..";
+
+const quotable = 'He said, "Hello". That\'s all folks.';
